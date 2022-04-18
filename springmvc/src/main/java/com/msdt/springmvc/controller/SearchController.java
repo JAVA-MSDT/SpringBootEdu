033425435e3d6f2b672963e034c305862e2802a9
Ahmed Samy <serenitydiver@hotmail.com>
2022-04-18 15:29:31 +0200

    Adding DeferredResult instead of Callable, Adding AsyncRequestTimeoutException to Exception Handler

--- springmvc/src/main/java/com/msdt/springmvc/controller/SearchController.java
+++ springmvc/src/main/java/com/msdt/springmvc/controller/SearchController.java
@@ -2,9 +2,11 @@ package com.msdt.springmvc.controller;
 
 import com.msdt.springmvc.entity.Product;
 import com.msdt.springmvc.service.ProductService;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,24 +17,33 @@ import java.util.concurrent.Callable;
 public class SearchController {
 
     private final ProductService productService;
+    private final AsyncTaskExecutor asyncTaskExecutor;
 
-    public SearchController(ProductService productService) {
+    public SearchController(ProductService productService, AsyncTaskExecutor asyncTaskExecutor) {
         this.productService = productService;
+        this.asyncTaskExecutor = asyncTaskExecutor;
     }
 
     @GetMapping("/search")
-    public Callable<ModelAndView> search(@RequestParam("search") String search, ModelAndView modelAndView, HttpServletRequest request) {
+    public DeferredResult<ModelAndView> search(@RequestParam("search") String search, ModelAndView modelAndView, HttpServletRequest request) {
+       DeferredResult<ModelAndView> deferredResult = new DeferredResult<>();
+
         modelAndView.setViewName("search:: " + search);
         modelAndView.setViewName("search");
         System.out.println("Async Flag: " + request.isAsyncSupported());
         System.out.println("Thread Name from Servlet container: " + Thread.currentThread().getName());
 
-        return () -> {
-            Thread.sleep(3000);
-            System.out.println("Thread Name from Task Execute: " + Thread.currentThread().getName());
+        asyncTaskExecutor.execute(() -> {
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread Name from DeferredResult: " + Thread.currentThread().getName());
             List<Product> products = productService.searchByName(search);
             modelAndView.addObject("products", products);
-            return modelAndView;
-        };
+             deferredResult.setResult(modelAndView);
+        });
+        return deferredResult;
     }
 }
--- springmvc/src/main/java/com/msdt/springmvc/exceptions/AppExceptionHandler.java
+++ springmvc/src/main/java/com/msdt/springmvc/exceptions/AppExceptionHandler.java
@@ -2,11 +2,12 @@ package com.msdt.springmvc.exceptions;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 
 @ControllerAdvice
 public class AppExceptionHandler {
 
-    @ExceptionHandler(AppException.class)
+    @ExceptionHandler({AppException.class, AsyncRequestTimeoutException.class})
     public String handleException() {
         return "error";
     }
